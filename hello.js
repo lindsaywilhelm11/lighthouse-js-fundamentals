5d8276bd789d4fb4ba8524e585642059490471e4
Lindsay Wilhelm <lindsaywilhelm11@gmail.com>
2021-11-02 18:54:56 +0000

    created two functions that say hello

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello  = function () {
+    console.log("Hello, world");
+  }
+  
+  sayHello();
+
+  const hello  = function (name) {
+    console.log("Hello, " + name);
+  }
+
+hello("Caliban");
+hello("Miranda");
+hello("Ferdinand");
